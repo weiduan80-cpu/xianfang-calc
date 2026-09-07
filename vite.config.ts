9ba539be9086d8117c6c6c1a24afee03ee341a45
weiduan80-cpu <wei.duan80@gmail.com>
2026-09-07 17:10:17 +0800

    chore: 构建配置(package.json/vite/tsconfig)

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import { viteSingleFile } from 'vite-plugin-singlefile'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [react(), viteSingleFile()],
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
